4e9a79aef23ed5a027c6f277373e8334fa5d6455
Muchiri-cmd <davismuchiri21@gmail.com>
2024-09-09 12:07:34 +0300

    Impliment Footer Section Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
+import { SectionWrapper } from "../wrapper"
+import { socialLinks } from "../info"
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div
+      className="text-center text-white text-[18px]"
+    >
+      <ul className="flex gap-10">
+        {socialLinks.map((link)=>(
+          <li key={link.name}
+            className="flex justify-center mt-8"
+          >
+            <a href={link.url} target="_blank" rel="noreferrer">
+              <i className={`${link.icon} text-white text-[24px]`}></i>
+            </a>
+          </li>
+          ))  
+        }
+      </ul>
+      <p>Made with Love ❤️‍🔥
+        <span className="text-yellow-300">Davis Muchiri</span>
+      </p>
     </div>
   )
 }
 
-export default Footer
+export default SectionWrapper(Footer,"footer")
